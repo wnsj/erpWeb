aac28a00e1a53debe53154d433a9fa45e5deb658
1332165192 <1332165192@qq.com>
2019-08-09 17:38:20 +0800

    新增返回任何格式过滤器

--- src/main.js
+++ src/main.js
@@ -10,7 +10,9 @@ import axios from 'axios';
 import moment from 'moment'
 
 import * as constant from '../src/assets/js/constant.js'
-import {exportTableToExcel} from 'vendor/Export2Excel.js'
+import {
+  exportTableToExcel
+} from 'vendor/Export2Excel.js'
 import * as date from '../src/assets/js/date.js'
 require('@/../static/js/bootstrap-datetimepicker.js')
 import global_date from '../src/assets/js/global_date'
@@ -49,72 +51,72 @@ Vue.prototype.isUseSetCookie = true
 
 
 
-Vue.prototype.notHaveRule='您没有此项功能操作的权限'
+Vue.prototype.notHaveRule = '您没有此项功能操作的权限'
 
 
 
 
 /*------------------------------------------公共方法-----------------------------------------------------------*/
-Vue.prototype.accountInfo = function(){
-	return constant.accountInfo()
+Vue.prototype.accountInfo = function() {
+  return constant.accountInfo()
 }
 Vue.prototype.getNowFormatDate = function() {
-	var date = new Date();
-	var seperator1 = "-";
-	var year = date.getFullYear();
-	var month = date.getMonth() + 1;
-	var strDate = date.getDate();
-	if (month >= 1 && month <= 9) {
-		month = "0" + month;
-	}
-	if (strDate >= 0 && strDate <= 9) {
-		strDate = "0" + strDate;
-	}
-	var currentdate = year + seperator1 + month + seperator1 + strDate;
-	return currentdate;
+  var date = new Date();
+  var seperator1 = "-";
+  var year = date.getFullYear();
+  var month = date.getMonth() + 1;
+  var strDate = date.getDate();
+  if (month >= 1 && month <= 9) {
+    month = "0" + month;
+  }
+  if (strDate >= 0 && strDate <= 9) {
+    strDate = "0" + strDate;
+  }
+  var currentdate = year + seperator1 + month + seperator1 + strDate;
+  return currentdate;
 };
 Vue.prototype.getCurrentDayAction = function(param) {
-	return date.timeInit(param)
+  return date.timeInit(param)
 }
 Vue.prototype.isBlank = function(param) {
-	return constant.isBlank(param)
+  return constant.isBlank(param)
 }
 Vue.prototype.jsGetAge = function(param) {
-	return date.jsGetAge(param)
+  return date.jsGetAge(param)
 }
 Vue.prototype.getYYYYMMDDHHMMSS_24 = function(param) {
-	return date.getYYYYMMDDHHMMSS_24(param)
+  return date.getYYYYMMDDHHMMSS_24(param)
 }
 Vue.prototype.getYYYYMMDDHHMMSS_00 = function(param) {
-	return date.getYYYYMMDDHHMMSS_00(param)
+  return date.getYYYYMMDDHHMMSS_00(param)
 }
 Vue.prototype.exportTableToExcel = function(tbId, fileName) {
-	if (!confirm("确定导出?")) {
-		return;
-	}
-	var myDate = new Date();
-	var year = myDate.getFullYear();
-	var month = myDate.getMonth() + 1;
-	var date = myDate.getDate();
-	exportTableToExcel(tbId, fileName + '_' + year + '_' + month + '_' + date);
+  if (!confirm("确定导出?")) {
+    return;
+  }
+  var myDate = new Date();
+  var year = myDate.getFullYear();
+  var month = myDate.getMonth() + 1;
+  var date = myDate.getDate();
+  exportTableToExcel(tbId, fileName + '_' + year + '_' + month + '_' + date);
 }
 Vue.prototype.has = function(param) {
-	return constant.has(param);
+  return constant.has(param);
 }
-Vue.prototype.getYYYY_MM_DD_T_HH_MM = function(param){
-	return date.getYYYY_MM_DD_T_HH_MM(param)
+Vue.prototype.getYYYY_MM_DD_T_HH_MM = function(param) {
+  return date.getYYYY_MM_DD_T_HH_MM(param)
 }
-Vue.prototype.getCurrentYYYY_MM_DD_HH_MM_SS = function(){
-	return date.getCurrentYYYY_MM_DD_HH_MM_SS();
+Vue.prototype.getCurrentYYYY_MM_DD_HH_MM_SS = function() {
+  return date.getCurrentYYYY_MM_DD_HH_MM_SS();
 }
 Vue.prototype.moment = function(targetDate, format) {
-	var formatStr = "YYYY-MM-DD HH:mm:ss"
-	var momentObj = moment();
-	if (!constant.isBlank(targetDate)) {
-		momentObj = moment(targetDate);
-	}
-	if (!constant.isBlank(format)) formatStr = format;
-	return momentObj.format(formatStr);
+  var formatStr = "YYYY-MM-DD HH:mm:ss"
+  var momentObj = moment();
+  if (!constant.isBlank(targetDate)) {
+    momentObj = moment(targetDate);
+  }
+  if (!constant.isBlank(format)) formatStr = format;
+  return momentObj.format(formatStr);
 }
 
 
@@ -125,44 +127,44 @@ Vue.prototype.moment = function(targetDate, format) {
  */
 //自定义指令v-has(不包含则删除该标签)
 Vue.directive('has', {
-	inserted: function(el, binding) {
-		if (!constant.has(binding.value)) {
-			el.parentNode.removeChild(el);
-		}
-	}
+  inserted: function(el, binding) {
+    if (!constant.has(binding.value)) {
+      el.parentNode.removeChild(el);
+    }
+  }
 });
 
 //路由卫士
 router.beforeEach((to, from, next) => {
-	consolelog(to, from);
-	if (to.path == '/login' && from.path == '/mainPage') {
-		next();
-	} else if (to.path == '/login') {
-		next();
-	} else {
-		let token = Cookies.get('accessToken');
-		let accountData = Cookies.get('accountData');
-		//consoleLogCookie(token,accountData);
-		if (constant.isBlank(token) || constant.isBlank(accountData)) {
-			next('/login');
-		} else if (to.path == '/') {
-			next('/mainPage');
-		} else if (to.path == '/login') {
-			next('/mainPage');
-		} else if (to.path == '/ERP/dist/index.html') {
-			next('/mainPage');
-		} else {
-			next();
-		}
-	}
+  consolelog(to, from);
+  if (to.path == '/login' && from.path == '/mainPage') {
+    next();
+  } else if (to.path == '/login') {
+    next();
+  } else {
+    let token = Cookies.get('accessToken');
+    let accountData = Cookies.get('accountData');
+    //consoleLogCookie(token,accountData);
+    if (constant.isBlank(token) || constant.isBlank(accountData)) {
+      next('/login');
+    } else if (to.path == '/') {
+      next('/mainPage');
+    } else if (to.path == '/login') {
+      next('/mainPage');
+    } else if (to.path == '/ERP/dist/index.html') {
+      next('/mainPage');
+    } else {
+      next();
+    }
+  }
 });
 
 function consolelog(to, from) {
-	console.log("to:" + to.path + ",from:" + from.path);
+  console.log("to:" + to.path + ",from:" + from.path);
 }
 
 function consoleLogCookie(token, accoutData) {
-	console.log("token:" + token + ",accoutData:" + accoutData)
+  console.log("token:" + token + ",accoutData:" + accoutData)
 }
 
 
@@ -172,10 +174,21 @@ function consoleLogCookie(token, accoutData) {
 /**
  * 时间过滤器
  */
-Vue.filter('dateFormat', function (time, formatStr='YYYY-MM-DD HH:mm') {
-  if(constant.isBlank(time)){
+Vue.filter('dateFormat', function(time, formatStr = 'YYYY-MM-DD HH:mm') {
+  if (constant.isBlank(time)) {
     return null
-  }else{
+  } else {
+    return moment(time).format(formatStr)
+  }
+})
+
+//时间过滤器可返回任何格式的时间
+Vue.filter('dateFormatFilter', function(time, format) {
+  var formatStr = 'YYYY-MM-DD HH:mm'
+  if (constant.isBlank(time)) {
+    return null;
+  } else {
+    formatStr = constant.isBlank(format) ? formatStr : format;
     return moment(time).format(formatStr)
   }
 })
@@ -196,17 +209,17 @@ Vue.prototype.hasDept = function(param) {
  * 创建VUE实例，其他实例注入
  */
 new Vue({
-	el: '#app',
-	store,
-	router,
-	data() {
-		return {
-			accountAccessToken: constant.isBlank(Cookies.get('accessToken')) ? '' : Cookies.get('accessToken'),
-			accountAccountData: constant.isBlank(Cookies.get('accountData')) ? '' : Cookies.get('accountData'),
-		}
-	},
-	components: {
-		App
-	},
-	template: '<App/>'
+  el: '#app',
+  store,
+  router,
+  data() {
+    return {
+      accountAccessToken: constant.isBlank(Cookies.get('accessToken')) ? '' : Cookies.get('accessToken'),
+      accountAccountData: constant.isBlank(Cookies.get('accountData')) ? '' : Cookies.get('accountData'),
+    }
+  },
+  components: {
+    App
+  },
+  template: '<App/>'
 })
