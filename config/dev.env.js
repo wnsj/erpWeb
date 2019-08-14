bd95eda7433b9fb8506c6159c64c2b851100de0a
1332165192 <1332165192@qq.com>
2019-08-14 15:22:49 +0800

    456465645

--- config/dev.env.js
+++ config/dev.env.js
@@ -6,6 +6,6 @@ module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
 	//API_HOST:'"http://172.16.213.210:8080/ERP "'
 	// API_HOST:'"http://172.16.2.203:8080/Erp"',
-	API_HOST:'"http://172.16.2.248:8080"'
-	// API_HOST:'"http://172.16.56.1:8080/Erp"'
+	//API_HOST:'"http://172.16.2.248:8080/Erp"'
+	API_HOST:'"http://172.16.56.1:8080"'
 })
