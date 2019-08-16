34dc2fb3df85b578f2387c5488a2ad868c2b530f
muwenliang <a562991986@163.com>
2019-08-16 10:13:57 +0800

    20190816

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,7 +4,7 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_HOST:'"http://172.16.2.203:8080"',
-  // API_HOST:'"http://172.16.2.248:8080"'
+  // API_HOST:'"http://172.16.2.203:8080"',
+  API_HOST:'"http://172.16.2.248:8080"'
   // API_HOST: '"http://172.16.56.1:8080"'
 })
