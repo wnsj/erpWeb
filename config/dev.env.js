b0275f61931f736c791191cf239906ae595f3f01
muwenliang <a562991986@163.com>
2019-08-16 10:28:05 +0800

    ----

--- config/dev.env.js
+++ config/dev.env.js
@@ -6,5 +6,5 @@ module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   API_HOST:'"http://172.16.2.203:8080"',
   API_HOST:'"http://172.16.2.248:8080"'
-  API_HOST: '"http://172.16.56.1:8080"'
+  // API_HOST: '"http://172.16.56.1:8080"'
 })
