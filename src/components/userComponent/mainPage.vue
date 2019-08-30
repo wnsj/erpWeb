d959f01d4718c31ee6b39f9ef4123d8d2fe26f4c
1332165192 <1332165192@qq.com>
2019-08-30 10:37:16 +0800

    修复关闭连接错误

--- src/components/userComponent/mainPage.vue
+++ src/components/userComponent/mainPage.vue
@@ -301,8 +301,8 @@
       }
       //监听窗口关闭事件，当窗口关闭时，主动去关闭websocket连接，防止连接还没断开就关闭窗口，server端会抛异常。
       window.onbeforeunload = function() {
-        this.closeWebSocket();
-        this.clearInterval();
+        vm.closeWebSocket();
+        vm.clearInterval();
       }
     },
     computed: {
