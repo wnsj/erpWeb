63be0610321fafa5c8074ffe498f95bf676973fe
muwenliang <a56299186@163.com>
2019-09-06 11:01:55 +0800

    ceshi

--- src/components/paperlessOffice/empRequire.vue
+++ src/components/paperlessOffice/empRequire.vue
@@ -12,7 +12,7 @@
 					<p>部门：</p>
 				</div>
 				<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
-					<depart></depart>
+					<depart  @departChange='departChange'></depart>
 				</div>
 			</div>
 
@@ -22,11 +22,12 @@
 					<p>申请时间：</p>
 				</div>
 				<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
+					
 					<span class="leavespan">
-						<datePicker v-model='beginDate'></datePicker>
+						<datePicker v-model='beginDate' v-on:change="dateAction('begin')"></datePicker>
 					</span> <span class="leavespan01">&nbsp;&nbsp;&nbsp;至：</span>
 					<span class="leavespan">
-						<datePicker v-model='endDate'></datePicker>
+						<datePicker v-model='endDate' v-on:change="dateAction('end')"></datePicker>
 					</span>
 				</div>
 			</div>
@@ -36,7 +37,7 @@
 					<p>需求职位：</p>
 				</div>
 				<div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
-					<position></position>
+					<position @positionChange='positionChange'></position>
 				</div>
 			</div>
 
@@ -44,10 +45,10 @@
 		<div class="row add-mt">
 			<button type="button" class="btn btn-primary pull-right" @click="dowmelxe('人员需求表')">导出</button>
 			<button type="button" class="btn btn-primary pull-right m_r_10" data-toggle="modal" v-on:click="showERInfo()">申请</button>
-			<button type="button" class="btn btn-warning pull-right m_r_10" data-toggle="modal">查询</button>
+			<button type="button" class="btn btn-warning pull-right m_r_10" data-toggle="modal" v-on:click="checkEMPRequirel()">查询</button>
 
 		</div>
-		<erApply></erApply>
+		<erApply @empRequireApply='feedbackApplyInfo'></erApply>
 		<div class="row">
 			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
 				<div class="table-responsive">
@@ -77,6 +78,34 @@
 								<th class="text-center">点击取消</th>
 							</tr>
 						</thead>
+						<tbody>
+							<tr v-for="(item,index) in erList" :key="index">
+								<td class="text-center">{{item.departmentName}}</td>
+								<td class="text-center">{{item.positionName}}</td>
+								<td class="text-center">{{item.applyTime}}</td>
+								<td class="text-center">{{item.needTime}}</td>
+								<td class="text-center">{{item.num}}</td>
+								<td class="text-center">{{item.applyReason}}</td>
+								<td class="text-center">{{item.shenchaName}}</td>
+								<td class="text-center">{{item.shenchaAdvice}}</td>
+								<td class="text-center">{{item.shenchaTime}}</td>
+								<td class="text-center">{{item.shenpiName}}</td>
+								<td class="text-center">{{item.shenpiAdvice}}</td>
+								<td class="text-center">{{item.shenpiTime}}</td>
+								<td class="text-center">{{item.renliName}}</td>
+								<td class="text-center">{{item.renliAdvice}}</td>
+								<td class="text-center">{{item.renliTime}}</td>
+								<td class="text-center">{{item.state}}</td>
+								<td class="text-center">{{item.renliName}}</td>
+								<td class="text-center">{{item.renliTime}}</td>
+								<td class="text-center">{{item.state}}</td>
+								<td class="text-center"><button type="button" class="btn btn-warning pull-right m_r_10" data-toggle="modal"
+									 	 >查看详情</button></td>
+								<td class="text-center"><button type="button" class="btn btn-warning pull-right m_r_10" data-toggle="modal"
+									 >点击取消</button></td>
+							</tr>
+						
+						</tbody>
 					</table>
 				</div>
 			</div>
@@ -85,6 +114,7 @@
 </template>
 
 <script>
+	import axios from 'axios'
 	import depart from '../../components/vuecommon/department.vue'
 	import position from '../../components/vuecommon/position.vue'
 	import erApply from '../paperlessOffice/subER/subERApply.vue'
@@ -98,15 +128,75 @@
 		},
 		data() {
 			return {
-				beginDate:this.moment(),
-				endDate:this.moment(),
+				beginDate:this.moment('','YYYY-MM-DD 00:00:00.000'),
+				endDate:this.moment('','YYYY-MM-DD 23:59:59.000'),
+				positionId:'0',
+				departId:'0',
+				erList:[],
 			};
 		},
 		methods:{
+			//跳转申请界面
 			showERInfo:function(){
 				$("#erApply").modal('show')
 				this.$children[4].initEMPRInfo()
-			}
+			},
+			//申请界面返回
+			feedbackApplyInfo:function(param){
+				if(param=='apply'){
+					
+				}
+			},
+			//UPDATE DATE
+			dateAction: function(param) {
+				if (param == 'begin') {
+					this.beginDate = this.moment(this.beginDate, 'YYYY-MM-DD 00:00:00.000')
+				} else if (param == 'end') {
+					this.endDate = this.moment(this.endDate, 'YYYY-MM-DD 23:59:59.000')
+				}
+			},
+			//feedback info of department
+			departChange:function(departId,departName){
+				this.departId=departId
+			},
+			//feedback info of position
+			positionChange:function(positionId,positionName){
+				this.positionId=positionId
+			},
+			//check applications of employee required
+			checkEMPRequirel:function(){
+				var url = this.url + '/wzbg/empRequireLsit'
+				console.log(url)
+				axios({
+						method: "post",
+						url: url,
+						headers: {
+							"Content-Type": this.contentType,
+							"Access-Token": this.accessToken
+						},
+						data: {
+							departId:this.departId,
+							positionId:this.positionId,
+							beginDate:this.beginDate,
+							endDate:this.endDate,
+						},
+						dataType: "json"
+					})
+					.then(response => {
+						var res = response.data
+						if (res.retCode == '0000') {
+							if (res.resData.length>0) {
+								console.log('erList'+res.resData)
+								this.erList=res.resData
+							}
+						} else {
+							alert(res.retMsg)
+						}
+					})
+					.catch(function(error) {
+						console.log("请求失败处理");
+					});
+			},
 		}
 	}
 </script>
